05ea8f29aa2861df0a6b141cd6621fb152b5baa7
peytonswan <98006928+peytonswan@users.noreply.github.com>
2024-04-02 01:38:04 -0700

    Create forfunscript.js

---
+++ forfunscript.js
@@ -0,0 +1,7 @@
+document.addEventListener('keydown', function(event) {
+    if (event.key === "g") { // When the "g" key is pressed
+        // Add class or trigger animation here
+        // For simplicity, we're just logging to the console
+        console.log('G key was pressed');
+    }
+});
